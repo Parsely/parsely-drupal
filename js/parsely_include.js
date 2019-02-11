fec772f1c674e06ad1416e84a33713d0eb14a6ba
Josh Romero <josh@parsely.com>
2019-02-11 12:57:42 -0600

    switch to config-free tracker

--- js/parsely_include.js
+++ js/parsely_include.js
@@ -2,17 +2,8 @@
     var site = drupalSettings.parsely['parsely-include'].apikey, // replace with the domain of your site (e.g. parsely.com)
         b = d.body,
         e = d.createElement("div");
-    e.innerHTML = '<span id="parsely-cfg" data-parsely-site="'+site+'"></span>';
-    e.id = "parsely-root";
-    e.style.display = "none";
+    e.id = 'parsely-cfg';
+    e.src = '//cdn.parsely.com/keys/'+site+'/p.js';
+    e.setAttribute('data-parsely-site', site);
     b.appendChild(e);
 })(document);
-(function(s, p, d) {
-    var h=d.location.protocol, i=p+"-"+s,
-        e=d.getElementById(i), r=d.getElementById(p+"-root"),
-        u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net"
-            :"static."+p+".com";
-    if (e) return;
-    e = d.createElement(s); e.id = i; e.async = true;
-    e.src = h+"//"+u+"/p.js"; r.appendChild(e);
-})("script", "parsely", document);
